44570a94182696576153751ef3317069244f8c25
michellehu0414 <michellehu0414@users.noreply.github.com>
2025-04-10 03:38:01 +0000

    deploy: e7f1527ea73f233ff6952510c2e41855890fc6b1

---
+++ assets/index.js
@@ -0,0 +1,1 @@
+import"./index.scss";import"@components/_global-components.scss";import{createAccordion}from"@components/Accordion/Accordion";document.addEventListener("DOMContentLoaded",(function(){createAccordion()}));
